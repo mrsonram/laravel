19d10f4b11f3e7bc52d5ce0034c62234fad6be73
chompoo24 <sonram_jumpatho@outlook.co.th>
2021-08-08 15:02:51 +0700

    8/8/2021

--- resources/views/manage/create.blade.php
+++ resources/views/manage/create.blade.php
@@ -6,6 +6,15 @@
 	{{ method_field('POST') }}
 
 	@include("manage/form")
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Post Image:</strong>
+             <input type="file" name="image" class="form-control" placeholder="Post Title">
+            @error('image')
+              <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+           @enderror
+        </div>
+    </div>
 
 	<div>
 		<button type="submit">Create</button>
--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -7,6 +7,7 @@
 <th>name</th>
 <th>type</th>
 <th>gender</th>
+<th>Image</th>
 <th>action</th>
 </tr>
 @foreach($animals as $animals)
@@ -15,6 +16,7 @@
 <td>{{ $animals->name }} </td>
 <td>{{ $animals->type }}</td>
 <td>{{ $animals->gender }}</td>
+<td><img src="{{ Storage::url($animals->pictures) }}" height="75" width="75" alt="" /></td>
 <td>
     <a href="{{ url('/') }}/home/{{ $animals->id }}">View</a>
     <a href="{{ url('/') }}/home/{{ $animals->id }}/edit">Edit</a>
