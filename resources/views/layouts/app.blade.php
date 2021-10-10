d7cc063b69f63edb70add2bbcadde3f25f94dd1b
Mr.Sonram Jumpatho <42561762+mrsonram@users.noreply.github.com>
2021-10-10 23:46:01 +0700

    10/10/2021

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item invisible">
+                                <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
