f3d469c90e78bfee34dbf8cb6b682a211f1604e2
Mr.Sonram Jumpatho <42561762+mrsonram@users.noreply.github.com>
2021-10-13 22:40:59 +0700

    Fix 0.3 (load page)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/manage';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
