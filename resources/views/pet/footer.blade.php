68784f2e01fc83d97100d65b1c3ff7e91f4259f9
Mr.Sonram Jumpatho <42561762+mrsonram@users.noreply.github.com>
2021-10-19 20:29:54 +0700

    Fix 0.3 (add detail on footer)

--- resources/views/pet/footer.blade.php
+++ resources/views/pet/footer.blade.php
@@ -6,23 +6,33 @@
             <div class="col mb-5 mb-lg-0">
                 <h2 class="text-uppercase mb-4">ที่อยู่</h2>
                 <p class="lead mb-0">
-                    <h3>เลขที่ 1 หมู่ 20 ต.คลองหนึ่ง
-                    <br />
-                    อ.คลองหลวง จ.ปทุมธานี 13180</h3>
+                <h3>มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์
+                    <br>
+                    เลขที่ 1 หมู่ 20 ต.คลองหนึ่ง อ.คลองหลวง จ.ปทุมธานี 13180
+                </h3>
                 </p>
             </div>
             <!-- Footer Social Icons-->
             <div class="col mb-5 mb-lg-0">
                 <h2 class="text-uppercase mb-4">ติดตามเพิ่มเติมได้ที่</h2>
-                <a class="btn btn-outline-light btn-social mx-1" href="#facebook"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#instagram"><i class="fab fa-fw fa-instagram"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#twitter"><i class="fab fa-fw fa-twitter"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#youtube"><i class="fab fa-fw fa-youtube"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#facebook"><i
+                        class="fab fa-fw fa-facebook-f"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#instagram"><i
+                        class="fab fa-fw fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#twitter"><i
+                        class="fab fa-fw fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#youtube"><i
+                        class="fab fa-fw fa-youtube"></i></a>
             </div>
         </div>
     </div>
 </footer>
 <!-- Copyright Section-->
 <div class="copyright py-4 text-center text-white">
-    <div class="container"><h3>Copyright VRU VET Project 2021</h3></div>
+    <div class="container">
+        <h3>Copyright VRU VET Project 2021
+            <br>พัฒนาระบบโดย นายศรราม จำปาโท รหัสนักศึกษา 59122420104
+            <br>คณะวิทยาศาสตร์ และเทคโนโลยี สาขาวิทยาการคอมพิวเตอร์
+        </h3>
+    </div>
 </div>
